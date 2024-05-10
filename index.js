682bd2240d9bf5c2bfd53733a51134a276e4aa41
Abdvllah7 <czczcsxvdg@gmail.com>
2024-05-10 06:14:13 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+let names = ['Shahid', 'Mubeen', 'Hanzala', 'Shees'];
+// loop through each name in the array
+for (let name of names) {
+    // print a personalize message for each friend
+    console.log(`Good morning ${name}, I hope you are having a fantastic day!`);
+}
---
+++ index.ts
@@ -0,0 +1,10 @@
+let names: string[]=['Shahid','Mubeen','Hanzala','Shees']
+
+ // loop through each name in the array
+
+ for(let name of names) {
+ // print a personalize message for each friend
+
+console.log(`Good morning ${name}, I hope you are having a fantastic day!`)
+
+}
